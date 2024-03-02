b15a307e759d50f4022ef5b3f64b57d2d34b4b2b
sekhar.kakarla12@gmail.com <sekhar.kakarla12@gmail.com>
2024-03-02 12:29:17 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng12';itemCount:number =4;
+  btnText:string='Add an item';
+  goalText:string ='my first life goal';
+}
